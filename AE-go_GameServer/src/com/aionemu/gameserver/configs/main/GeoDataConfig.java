88ad2e9978fcb694c0c11a364e7746e9a6d456b2
Destinywars <Destinywars@5f6b67ec-ab53-48ec-8083-8baf883bdfaa>
2010-10-13 22:08:46 +0000

    Renaming Folders, AE-go to AL.

--- AE-go_GameServer/src/com/aionemu/gameserver/configs/main/GeoDataConfig.java
+++
@@ -1,7 +0,0 @@
-package com.aionemu.gameserver.configs.main;
-import com.aionemu.commons.configuration.Property;
-public class GeoDataConfig
-{
-    @Property(key = "gameserver.geodata.enable", defaultValue = "false")
-    public static boolean GEO_ENABLE;
-}
