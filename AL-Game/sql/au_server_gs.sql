c11bbef98da9080b1caf0573d1dec8b29122a1a6
Destinywars <Destinywars@5f6b67ec-ab53-48ec-8083-8baf883bdfaa>
2010-10-14 02:18:12 +0000

    Portal cooldown added in au_server_gs.sql

--- AL-Game/sql/au_server_gs.sql
+++ AL-Game/sql/au_server_gs.sql
@@ -487,13 +487,13 @@ CREATE TABLE IF NOT EXISTS  `player_pets` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Table structure for 'item_cooldowns'
+-- `portal_cooldowns`
 -- ----------------------------
-CREATE TABLE IF NOT EXISTS `item_cooldowns` (
-`player_id` int(11) NOT NULL,
-`delay_id` int(11) NOT NULL,
-`use_delay` SMALLINT UNSIGNED NOT NULL,
-`reuse_time` BIGINT(13) NOT NULL,
-PRIMARY KEY (`player_id`, `delay_id`),
-FOREIGN KEY (`player_id`) REFERENCES `players` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE IF NOT EXISTS `portal_cooldowns` (
+  `player_id` int(11) NOT NULL,
+  `world_id` int(11) NOT NULL,
+  `use_delay` int(11) NOT NULL,
+  `reuse_time` BIGINT(13) NOT NULL,
+  PRIMARY KEY (`player_id`, `world_id`),
+  FOREIGN KEY (`player_id`) REFERENCES `players` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+  ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
